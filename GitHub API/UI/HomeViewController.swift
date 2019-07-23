17a3fa2bfbb4515a77ea5eafdbdf9be9dc54021a
YIFU RONG <yifu.rong@hotmail.com>
2019-07-23 18:06:24 -0400

    Add HomeViewController and remove the default UI components

---
+++ GitHub API/UI/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  GitHub API
+//
+//  Created by YIFU RONG on 7/23/19.
+//  Copyright © 2019 Yifu Rong. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.view.backgroundColor = .red
+    }
+}
