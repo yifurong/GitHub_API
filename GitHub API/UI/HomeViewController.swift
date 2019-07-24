9aceee9d41c5fe5daee858ba66664b07a3103307
YIFU RONG <yifu.rong@hotmail.com>
2019-07-23 20:59:00 -0400

    Add owner and repo textfield, allow user searching commits

--- GitHub API/UI/HomeViewController.swift
+++ GitHub API/UI/HomeViewController.swift
@@ -12,18 +12,53 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     private var commits: [CommitResponse] = []
     
+    private let ownerTextField = UITextField()
+    private let repoTextField = UITextField()
+    private let retrieveButton = UIButton()
     private let tableView = UITableView()
     
     override func loadView() {
         super.loadView()
         
         self.view.backgroundColor = .white
+        self.edgesForExtendedLayout = []
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        self.tableView.keyboardDismissMode = .onDrag
+        
+        self.ownerTextField.placeholder = "yifurong"
+        self.repoTextField.placeholder = "GitHub_API"
+        [self.ownerTextField, self.repoTextField].forEach({
+            $0.layer.borderColor = UIColor.black.cgColor
+            $0.layer.borderWidth = 1
+        })
+        
+        self.retrieveButton.setTitle("Retrieve", for: .normal)
+        self.retrieveButton.addTarget(self, action: #selector(retrieveClicked(_:)), for: .touchUpInside)
+        self.retrieveButton.setTitleColor(.black, for: .normal)
+        
+        self.view.addSubview(view: self.ownerTextField) { (make) in
+            make.top.left.equalToSuperview().offset(10.0)
+            make.width.equalToSuperview().multipliedBy(0.25)
+            make.height.equalTo(25.0)
+        }
+        
+        self.view.addSubview(view: self.repoTextField) { (make) in
+            make.top.width.height.equalTo(self.ownerTextField)
+            make.left.equalTo(self.ownerTextField.snp.right).offset(15.0)
+        }
+        
+        self.view.addSubview(view: self.retrieveButton) { (make) in
+            make.top.width.height.equalTo(self.ownerTextField)
+            make.left.equalTo(self.repoTextField.snp.right).offset(15.0)
+            make.right.equalToSuperview().offset(-10.0)
+        }
         
         self.view.addSubview(view: self.tableView) { (make) in
-            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+            make.top.equalTo(self.ownerTextField.snp.bottom).offset(10.0)
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
         }
     }
     
@@ -54,6 +89,9 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     private func loadCommits(owner: String, repo: String) {
         APIManager.GitHubAPI.fetchRecentCommits(owner: owner, repo: repo) { (response, error) in
             guard error == nil else {
+                self.commits = []
+                self.tableView.reloadData()
+                
                 let controller = UIAlertController(title: "Oops!", message: "Invalid owner or repo", preferredStyle: .alert)
                 let action = UIAlertAction(title: "OK", style: .default, handler: nil)
                 controller.addAction(action)
@@ -63,5 +101,12 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
             self.commits = response
             self.tableView.reloadData()
         }
+    }
+    
+    @objc func retrieveClicked(_ sender: UIButton) {
+        let owner: String! = self.ownerTextField.text?.isEmpty ?? true ? self.ownerTextField.placeholder : self.ownerTextField.text
+        let repo: String! = self.repoTextField.text?.isEmpty ?? true ? self.repoTextField.placeholder : self.repoTextField.text
+        
+        self.loadCommits(owner: owner, repo: repo)
     }
 }
